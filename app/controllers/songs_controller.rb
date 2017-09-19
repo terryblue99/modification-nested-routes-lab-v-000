38332b407d82fdbeff51778d3374c9bfbc892520
Terry Blue <terryblue@comcast.net>
2017-09-19 22:03:35 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,11 +46,11 @@ class SongsController < ApplicationController
   def edit
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
+      @artist_id = params[:artist_id]
       if artist.nil?
         redirect_to artists_path, alert: "Artist not found."
       else
         @song = artist.songs.find_by(id: params[:id])
-        @artist_id = params[:artist_id]
         redirect_to artist_songs_path, alert: "Song not found." if @song.nil?
       end
     else
